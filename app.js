2fc4a809f100eba6bb9649b355867a6d7c9a9ba1
Keisi Polli <keisi.polli@khk.ee>
2021-11-11 00:10:41 +0200

    Created addBook functionality

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ const booksList = document.querySelector('#books-list');
 
 // events
 form.addEventListener('submit', addBook);
-booksList.addEventListener('click', deleteBook);
-
+//booksList.addEventListener('click', deleteBook);
+//document.addEventListener('DOMContentLoaded', getBooksFromLocalStorage);
 
 function addBook(event){
     // get form input data
@@ -17,21 +17,35 @@ function addBook(event){
     let author = authorInput.value;
     let isbn = isbnInput.value;
 
-    // create book
-    const book = [title, author, isbn]
     // create <tr> element
     const tr = document.createElement('tr');
-    for(let i = 0; i < book.length; i++){
-        // create <td> element
-        let td = document.createElement('td');
-        // create text element
-        let text = document.createTextNode(book[i]);
-        // add text to <td>
-        td.appendChild(text);
-        // add td to tr
-        tr.appendChild(td);// add td to tr
-        tr.appendChild(td);
-    }
+    // title
+    // create <td> element
+    let td = document.createElement('td');
+    // create text element
+    let text = document.createTextNode(title);
+    // add text to <td>
+    td.appendChild(text);
+    // add td to tr
+    tr.appendChild(td);
+    // author
+    // create <td> element
+    td = document.createElement('td');
+    // create text element
+    text = document.createTextNode(author);
+    // add text to <td>
+    td.appendChild(text);
+    // add td to tr
+    tr.appendChild(td);
+    // isbn
+    // create <td> element
+    td = document.createElement('td');
+    // create text element
+    text = document.createTextNode(isbn);
+    // add text to <td>
+    td.appendChild(text);
+    // add td to tr
+    tr.appendChild(td);
     // X link
     // create <td> element
     td = document.createElement('td');
@@ -47,21 +61,87 @@ function addBook(event){
     tr.appendChild(td);
     // add tr to tbody
     booksList.appendChild(tr);
-    // save book
-    addBookToLocalStorage(book);
+    // save task
+    //addTaskToLocalStorage(task);
     titleInput.value = '';
     authorInput.value = '';
     isbnInput.value = '';
+
     event.preventDefault();
 }
 
-function deleteBook(event){
-    if(event.target.textContent === 'X'){
-        if(confirm('Do you want to delete this book?')){
-            event.target.parentElement.parentElement.remove();
-            //let bookTitle = event.target.parentElement.parentElement.firstChild.textContent;
-            let bookISBN = event.target.parentElement.previousElementSibling.textContent;
-            deleteBookFromLocalStorage(bookISBN);
-        }
+/*
+function getTasksFromLocalStorage(){
+    let tasks;
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
     }
+    tasks.forEach(function (tasksElement){
+        // create <li> element
+        const li = document.createElement('li');
+        // add css class
+        li.className = 'collection-item';
+        // create text element
+        const text = document.createTextNode(tasksElement);
+        // add text to <li>
+        li.appendChild(text);
+        // create <a> element
+        const link = document.createElement('a');
+        // add css class
+        link.className = 'secondary-content';
+        // set href atribute to <a>
+        link.setAttribute('href', '#');
+        // add text content to <a>
+        link.appendChild(document.createTextNode('X'));
+        // add <a> to <li>
+        li.appendChild(link);
+        // add li to ul
+        const ul = document.querySelector('.collection');
+        ul.appendChild(li);
+    });
 }
+*/
+
+/*
+function deleteBook(event){
+   if(event.target.textContent === 'X'){
+       if(confirm('Do you want to delete this task?')){
+           event.target.parentElement.remove();
+           task = event.target.parentElement.firstChild.textContent;
+           deleteTaskFromLocalStorage(task);
+       }
+   }
+}
+*/
+/*
+function deleteBookFromLocalStorage(task){
+    let tasks;
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    tasks.forEach(function (tasksElement, index){
+        if(tasksElement === task){
+            tasks.splice(index, 1);
+        }
+    });
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+*/
+
+
+/*
+function addTaskToLocalStorage(task){
+    let tasks;
+    if(localStorage.getItem('tasks') === null){
+        tasks = [];
+    } else {
+        tasks = JSON.parse(localStorage.getItem('tasks'));
+    }
+    tasks.push(task);
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+*/
